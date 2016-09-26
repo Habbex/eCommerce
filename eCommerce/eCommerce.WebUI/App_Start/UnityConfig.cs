f20419ce8a3ba5fc15161de6e9dfae4df9ae563c
Ehab <Ihabokal@hotmail.com>
2016-09-26 16:56:18 +0200

    repairing

--- eCommerce/eCommerce.WebUI/App_Start/UnityConfig.cs
+++ eCommerce/eCommerce.WebUI/App_Start/UnityConfig.cs
@@ -1,9 +1,6 @@
 using System;
 using Microsoft.Practices.Unity;
 using Microsoft.Practices.Unity.Configuration;
-using eCommerce.Contracts.Repositories;
-using eCommerce.Model;
-using eCommerce.DAL.Repositories;
 
 namespace eCommerce.WebUI.App_Start
 {
@@ -31,7 +28,7 @@ namespace eCommerce.WebUI.App_Start
 
         /// <summary>Registers the type mappings with the Unity container.</summary>
         /// <param name="container">The unity container to configure.</param>
-        /// <remarks>There is no need to register concrete types such as controllers or API controllers (unless you want to
+        /// <remarks>There is no need to register concrete types such as controllers or API controllers (unless you want to 
         /// change the defaults), as Unity allows resolving a concrete type even if it was not previously registered.</remarks>
         public static void RegisterTypes(IUnityContainer container)
         {
@@ -40,13 +37,6 @@ namespace eCommerce.WebUI.App_Start
 
             // TODO: Register your types here
             // container.RegisterType<IProductRepository, ProductRepository>();
-            container.RegisterType<IRepositoryBase<Customer>, CustomerRepository>();
-            container.RegisterType<IRepositoryBase<Product>, ProductRepository>();
-            container.RegisterType<IRepositoryBase<Basket>, BasketRepository>();
-            container.RegisterType<IRepositoryBase<Voucher>, VoucherRepository>();
-            container.RegisterType<IRepositoryBase<VoucherType>, VoucherTypeRepository>();
-            container.RegisterType<IRepositoryBase<BasketVoucher>, BasketVoucherRepository>();
-            container.RegisterType<IRepositoryBase<BasketItem>, BasketItemsRepository>();
         }
     }
 }
